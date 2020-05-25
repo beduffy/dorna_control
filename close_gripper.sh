453da95f84cde3b21d601ee7c95111468c501cd9
Ben Duffy <benfduffy@gmail.com>
2020-05-25 19:35:15 +0100

    Deactivated conda correctly for nano and sourced right devel

--- close_gripper.sh
+++ close_gripper.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 echo $PWD
 echo $1
-source activate py27
-source /home/beduffy/all_projects/arm_control_ros/devel/setup.bash
+source ~/archiconda3/etc/profile.d/conda.sh
+#source /home/beduffy/all_projects/arm_control
+#source activate py27
+
+conda deactivate
+source /home/ben/all_projects/arm_control_ros/devel/setup.bash
+
+echo $(which python)
 #python /home/beduffy/all_projects/arm_control_ros/src/arm_control/control/scripts/rosservice_call_servo_gripper.py -m $2
 python /home/ben/all_projects/arm_control_ros/src/arm_control/control/scripts/rosservice_call_servo_gripper.py -m $2
