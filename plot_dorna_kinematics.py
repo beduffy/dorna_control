8e9addce0fa5e5c8a628cc3d371a85779836fcae
beduffy <benfduffy@gmail.com>
2021-11-14 17:36:32 +0100

    Copied and improved FK from Dorna for plotting in Open3D and debugging
    - IK there too
    - inch to mm
    - line mesh file there
    - create_sphere_at_pos
    - coordinate frames
    - solve wrist mystery
    - toolhead in mm not m
    - theta in degrees
    - usual side x and then xyz conversion
    - better dictionary output

---
+++ plot_dorna_kinematics.py
@@ -0,0 +1,301 @@
+import math
+
+import numpy as np
+try:
+    import open3d as o3d
+    from skimage.measure import find_contours
+except Exception as e:
+    print(e)
+    print('Tried to import open3d or skimage but not installed')
+
+from line_mesh import LineMesh
+
+def _inch_to_mm(x):
+    if x == None:
+        return None
+    return x * 25.4
+
+
+def create_sphere_at_pos(pos, radius=4, color=[1.0, 0.0, 0.0]):
+    centroid_sphere = o3d.geometry.TriangleMesh.create_sphere(radius=radius)
+    centroid_sphere.compute_vertex_normals()
+    centroid_sphere.paint_uniform_color(color)
+    centroid_sphere.translate(pos)
+
+    return centroid_sphere
+
+def plot_open3d_Dorna(joint_and_link_positions, extra_geometry_elements=None):
+    # todo how to make more accurate 3d model? URDF/meshes?
+
+    print("Drawing Open3D model of arm with lines")
+
+    # starting_x = joint_and_link_positions["starting_x"]
+    # starting_y = joint_and_link_positions["starting_y"]
+    # starting_z = joint_and_link_positions["starting_z"]
+    x1 = joint_and_link_positions["shoulder"][0]
+    y1 = joint_and_link_positions["shoulder"][1]
+    z1 = joint_and_link_positions["shoulder"][2]
+    x2 = joint_and_link_positions["elbow"][0]
+    y2 = joint_and_link_positions["elbow"][1]
+    z2 = joint_and_link_positions["elbow"][2]
+    x3 = joint_and_link_positions["wrist"][0]
+    y3 = joint_and_link_positions["wrist"][1]
+    z3 = joint_and_link_positions["wrist"][2]
+    x4 = joint_and_link_positions["toolhead"][0]
+    y4 = joint_and_link_positions["toolhead"][1]
+    z4 = joint_and_link_positions["toolhead"][2]
+
+    # s_x = joint_and_link_positions["s_x"]
+    # e_x = joint_and_link_positions["e_x"]
+    # w_x = joint_and_link_positions["w_x"]
+    # wrist_pitch_starting_side_x = joint_and_link_positions['wrist_pitch_starting_side_x']
+    # wrist_pitch_starting_x = joint_and_link_positions['wrist_pitch_starting_x']
+    # wrist_pitch_starting_y = joint_and_link_positions['wrist_pitch_starting_y']
+    # wrist_pitch_starting_z = joint_and_link_positions['wrist_pitch_starting_z']
+
+    # gripper model
+    # gripper_back_centre_x = joint_and_link_positions.get('gripper_back_centre_x')
+    # if gripper_back_centre_x:
+    #     gripper_back_centre_y = joint_and_link_positions.get('gripper_back_centre_y')
+    #     gripper_back_centre_z = joint_and_link_positions.get('gripper_back_centre_z')
+    #     gripper_left_jaw_start_x = joint_and_link_positions.get('gripper_left_jaw_start_x')
+    #     gripper_left_jaw_start_y = joint_and_link_positions.get('gripper_left_jaw_start_y')
+    #     gripper_left_jaw_start_z = joint_and_link_positions.get('gripper_left_jaw_start_z')
+    #     gripper_left_jaw_end_x = joint_and_link_positions.get('gripper_left_jaw_end_x')
+    #     gripper_left_jaw_end_y = joint_and_link_positions.get('gripper_left_jaw_end_y')
+    #     gripper_left_jaw_end_z = joint_and_link_positions.get('gripper_left_jaw_end_z')
+    #     gripper_right_jaw_start_x = joint_and_link_positions.get('gripper_right_jaw_start_x')
+    #     gripper_right_jaw_start_y = joint_and_link_positions.get('gripper_right_jaw_start_y')
+    #     gripper_right_jaw_start_z = joint_and_link_positions.get('gripper_right_jaw_start_z')
+    #     gripper_right_jaw_end_x = joint_and_link_positions.get('gripper_right_jaw_end_x')
+    #     gripper_right_jaw_end_y = joint_and_link_positions.get('gripper_right_jaw_end_y')
+    #     gripper_right_jaw_end_z = joint_and_link_positions.get('gripper_right_jaw_end_z')
+    #     gripper_tip_x = joint_and_link_positions.get('gripper_tip_x')
+    #     gripper_tip_y = joint_and_link_positions.get('gripper_tip_y')
+    #     gripper_tip_z = joint_and_link_positions.get('gripper_tip_z')
+
+    # optional IK target visualisation
+    # x = joint_and_link_positions.get("x")
+    # y = joint_and_link_positions.get("y")
+    # z = joint_and_link_positions.get("z")
+    # side_view_x_target = joint_and_link_positions.get("side_view_x_target")
+
+    # start creating points, lines, colors and spheres to visualise model
+    # points = [[starting_x, starting_y, starting_z],
+    points = [[x1, y1, z1],
+              [x2, y2, z2],
+            #   [wrist_pitch_starting_x, wrist_pitch_starting_y, wrist_pitch_starting_z],
+              [x3, y3, z3],
+              [x4, y4, z4],
+            #   [gripper_back_centre_x, gripper_back_centre_y, gripper_back_centre_z],
+            #   [gripper_left_jaw_start_x, gripper_left_jaw_start_y, gripper_left_jaw_start_z],
+            #   [gripper_right_jaw_start_x, gripper_right_jaw_start_y, gripper_right_jaw_start_z],
+            #   [gripper_left_jaw_end_x, gripper_left_jaw_end_y, gripper_left_jaw_end_z],
+            #   [gripper_right_jaw_end_x, gripper_right_jaw_end_y, gripper_right_jaw_end_z],
+            #   [gripper_tip_x, gripper_tip_y, gripper_tip_z],
+              ]
+
+    # lines = [[0, 1], [1, 2], [2, 3], [3, 4], [5, 6], [5, 7], [6, 8], [7, 9], [4, 10]]
+    lines = [[0, 1], [1, 2], [2, 3]]
+    # lines = [[i, i + 1] for i, x in enumerate(points[:-1])]
+    # colors = [[1, 0, 0] for i in range(len(lines))]
+    colors = [[0, 0, 0], [1, 0, 0], [1, 1, 0]]
+
+    line_set = o3d.geometry.LineSet()
+    line_set.points = o3d.utility.Vector3dVector(points)
+    line_set.lines = o3d.utility.Vector2iVector(lines)
+    line_set.colors = o3d.utility.Vector3dVector(colors)
+
+    # Create Line Mesh
+    line_mesh1 = LineMesh(points, lines, colors, radius=1.5)
+    line_mesh1_geoms = line_mesh1.cylinder_segments
+
+    all_spheres = []
+    for p in points:
+        mesh_sphere = create_sphere_at_pos(np.array(p), color=[0.1, 0.1, 0.7], radius=3.0)
+        all_spheres.append(mesh_sphere)
+
+    list_of_geometry_elements = [line_set, *line_mesh1_geoms] + all_spheres
+    if extra_geometry_elements:
+        list_of_geometry_elements.extend(extra_geometry_elements)
+    o3d.visualization.draw_geometries(list_of_geometry_elements)  # todo eventually nonblocking version
+
+"""
+inverse kinematics: xyz to joint
+"""
+def i_k(self, xyz):
+    try:
+        x = xyz[0]
+        y = xyz[1]
+        z = xyz[2]
+        
+        if self._config["unit"]["length"] == "mm":
+            x = self._mm_to_inch(x)
+            y = self._mm_to_inch(y)
+            z = self._mm_to_inch(z)			
+        
+
+        alpha = xyz[3]
+        beta = xyz[4]
+
+        alpha = math.radians(alpha)
+        beta = math.radians(beta)
+
+        # first we find the base rotation
+        teta_0 = math.atan2(y, x)
+
+        # next we assume base is not rotated and everything lives in x-z plane
+        x = math.sqrt(x ** 2 + y ** 2)
+
+        # next we update x and z based on base dimensions and hand orientation
+        x -= (self._bx + self._config["toolhead"]["x"] * math.cos(alpha))
+        z -= (self._bz + self._config["toolhead"]["x"] * math.sin(alpha))
+
+        # at this point x and z are the summation of two vectors one from lower arm and one from upper arm of lengths l1 and l2
+        # let L be the length of the overall vector
+        # we can calculate the angle between l1 , l2 and L
+        L = math.sqrt(x ** 2 + z ** 2)
+        L = np.round(L,13) # ???
+        # not valid
+        if L > (self._l1 + self._l2) or self._l1 > (self._l2 + L) or self._l2 > (self._l1 + L):  # in this case there is no solution
+            return None
+
+        # init status
+        status = 0
+        if L > (self._l1 + self._l2) - self._delta_e or self._l1 > (self._l2 + L) - self._delta_e: # in this case there is no solution
+            status = 1
+
+        teta_l1_L = math.acos((self._l1 ** 2 + L ** 2 - self._l2 ** 2) / (2 * self._l1 * L))  # l1 angle to L
+        teta_L_x = math.atan2(z, x)  # L angle to x axis
+        teta_1 = teta_l1_L + teta_L_x
+        # note that the other solution would be to set teta_1 = teta_L_x - teta_l1_L. But for the dynamics of the robot the first solution works better.
+        teta_l1_l2 = math.acos((self._l1 ** 2 + self._l2 ** 2 - L ** 2) / (2 * self._l1 * self._l2))  # l1 angle to l2
+        teta_2 = teta_l1_l2 - math.pi
+        teta_3 = alpha - teta_1 - teta_2
+        teta_4 = beta
+        teta_0 = math.degrees(teta_0)
+        teta_1 = math.degrees(teta_1)
+        teta_2 = math.degrees(teta_2)
+        teta_3 = math.degrees(teta_3)
+        teta_4 = math.degrees(teta_4)
+
+        return [teta_0, teta_1, teta_2, teta_3, teta_4] + xyz[5:]
+
+    except:
+        return None
+
+"""
+forward kinematics: joint to xyz
+"""
+def f_k(joint_angles_deg):
+    try:
+
+        # TODO change all variable names
+        # TODO understand IK too
+        # TODO would be awesome to do matplotlib pointcloud as well
+        # TODO commit and push all to repo
+
+        _bx = _inch_to_mm(3.759)  # x-distance from base to shoulder. 95.47859999999999mm Most likely.
+        _bz = _inch_to_mm(8.111)  # height from ground to shoulder joint. 206.01940000000002mm confirmed
+        l_shoulder_to_elbow = _inch_to_mm(8.)  # distance from shoulder to elbow. 203.2mm confirmed
+        l_elbow_to_wrist = _inch_to_mm(6.)  # TODO: maybe distance from base to shoulder. NO. From elbow to wrist
+        # toolhead_x_length = 0.175
+        toolhead_x_length = 175  # in mm, like everything else
+        print('_bx, _bz, l_shoulder_to_elbow, l_elbow_to_wrist')
+        print(_bx, _bz, l_shoulder_to_elbow, l_elbow_to_wrist)
+
+        # joint to radian
+        theta = [math.radians(j) for j in joint_angles_deg]
+
+        # import pdb;pdb.set_trace()
+
+        # first we find x, y, z assuming base rotation is zero (theta_0 = 0). Then we rotate everything
+        # then we rotate the robot around z axis for theta_0
+
+        # top-down view
+        # tmp = _bx + _l1 * math.cos(theta[1]) + _l2 * math.cos(theta[1] + theta[2]) + toolhead_x_length * math.cos(theta[1] + theta[2] + theta[3])
+        # x = tmp * math.cos(theta[0])
+        # y = tmp * math.sin(theta[0])
+
+        # base_x = TODO ???
+
+        # TODO we're missing distance from elbow to wrist?!?!?
+        # TODO why not negative bx? Because x is forward
+        shoulder_side_x = _bx
+        # shoulder_side_x = _bx + l_shoulder_to_elbow * math.cos(theta[1])
+        elbow_side_x = shoulder_side_x + l_shoulder_to_elbow * math.cos(theta[1])
+        # elbow_side_x = shoulder_side_x + l_elbow_to_wrist * math.cos(theta[1] + theta[2])
+        wrist_side_x = elbow_side_x + l_elbow_to_wrist * math.cos(theta[1] + theta[2])
+        toolhead_side_x = elbow_side_x + toolhead_x_length * math.cos(theta[1] + theta[2] + theta[3])
+        print('All side x: {} {} {}'.format(shoulder_side_x, elbow_side_x, toolhead_side_x))
+        # rotate around z-axis
+        shoulder_x = shoulder_side_x * math.cos(theta[0])
+        shoulder_y = shoulder_side_x * math.sin(theta[0])
+        elbow_x = elbow_side_x * math.cos(theta[0])
+        elbow_y = elbow_side_x * math.sin(theta[0])
+        wrist_x = wrist_side_x * math.cos(theta[0])
+        wrist_y = wrist_side_x * math.sin(theta[0])
+        toolhead_x = toolhead_side_x * math.cos(theta[0])
+        toolhead_y = toolhead_side_x * math.sin(theta[0])
+        print('shoulder_x: {} shoulder_y: {} elbow_x: {} elbow_y: {} toolhead_x: {} toolhead_y: {}'.format(shoulder_x, shoulder_y, elbow_x, elbow_y, toolhead_x, toolhead_y))
+
+        # x = tmp * math.cos(theta[0])
+        # y = tmp * math.sin(theta[0])
+
+        # side-view
+        # z = _bz + _l1 * math.sin(theta[1]) + _l2 * math.sin(theta[1] + theta[2]) + toolhead_x_length * math.sin(theta[1] + theta[2] + theta[3])
+        # shoulder_z = _bz + l_shoulder_to_elbow * math.sin(theta[1])
+        shoulder_z = _bz
+        elbow_z = shoulder_z + l_shoulder_to_elbow * math.sin(theta[1])
+        # elbow_z = shoulder_z + l_elbow_to_wrist * math.sin(theta[1] + theta[2])
+        wrist_z = elbow_z + l_elbow_to_wrist * math.sin(theta[1] + theta[2])
+        toolhead_z = elbow_z + toolhead_x_length * math.sin(theta[1] + theta[2] + theta[3])
+
+        alpha = theta[1] + theta[2] + theta[3]
+        beta = theta[4]  # just wrist roll right?
+        alpha = math.degrees(alpha)
+        beta = math.degrees(beta)
+
+        xyz_toolhead_pos = [toolhead_x, toolhead_y, toolhead_z]
+        full_toolhead_fk = xyz_toolhead_pos + [alpha, beta] + joint_angles_deg[5:]
+        xyz_positions_of_all_joints = {'shoulder': [shoulder_x, shoulder_y, shoulder_z], 
+                                       'elbow': [elbow_x, elbow_y, elbow_z], 
+                                       'wrist': [wrist_x, wrist_y, wrist_z], 
+                                       'toolhead': xyz_toolhead_pos}
+
+        # TODO do I need to do this? What about it makes it in inches? ahh bx and all?
+        # if _config["unit"]["length"] == "mm":
+        #     _rtn = [_inch_to_mm(c) for c in _rtn]
+
+        return full_toolhead_fk, xyz_positions_of_all_joints
+
+    except Exception as e:
+        print(e)
+        return None
+
+# base, shoulder, elbow, wrist pitch, wrist roll
+# full_toolhead_fk, xyz_positions_of_all_joints = f_k([0.0, 0.0, 0.0, 0.0, 0.0])
+# full_toolhead_fk, xyz_positions_of_all_joints = f_k([0.0, 0.0, 0.0, 30.0, 0.0])
+# full_toolhead_fk, xyz_positions_of_all_joints = f_k([0.0, 0.0, 0.0, 45, 0.0])
+full_toolhead_fk, xyz_positions_of_all_joints = f_k([0.0, 0.0, 0.0, 60, 0.0])
+# full_toolhead_fk, xyz_positions_of_all_joints = f_k([45, 0.0, 0.0, 0.0, 0.0])
+# full_toolhead_fk, xyz_positions_of_all_joints = f_k([45, 45, 45, 0.0, 0.0])
+# full_toolhead_fk, xyz_positions_of_all_joints = f_k([45, 45, 45, 60, 70])
+print('full_toolhead_fk')
+print(full_toolhead_fk)
+print('xyz_positions_of_all_joints')
+print(xyz_positions_of_all_joints)
+
+# origin_frame_size = 0.01
+# origin_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+#     size=origin_frame_size, origin=[0.0, 0.0, 0.0])
+coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+    size=25.0, origin=[0.0, 0.0, 0.0])
+
+# origin_frame_size = 0.01
+# origin_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+#     size=origin_frame_size, origin=[0.0, 0.0, 0.0])
+coordinate_frame_shoulder_height = o3d.geometry.TriangleMesh.create_coordinate_frame(
+    size=25.0, origin=[0.0, 0.0, 206.01940000000002])
+
+plot_open3d_Dorna(xyz_positions_of_all_joints, extra_geometry_elements=[coordinate_frame, coordinate_frame_shoulder_height])
