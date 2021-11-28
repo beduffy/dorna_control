8ca8caa136d8b2155a023d5aaad70a7eb6722648
beduffy <benfduffy@gmail.com>
2021-11-28 03:05:58 +0100

    Added extra.py for random_colors

---
+++ lib/extra.py
@@ -0,0 +1,21 @@
+import random
+import time
+import colorsys
+
+try:
+    import matplotlib.pyplot as plt
+except Exception as e:
+    print(e)
+    print('Tried to import matplotlib but not installed')
+
+def random_colors(N, bright=True):
+    """
+    Generate random colors.
+    To get visually distinct colors, generate them in HSV space then
+    convert to RGB.
+    """
+    brightness = 1.0 if bright else 0.7
+    hsv = [(i / N, 1, brightness) for i in range(N)]
+    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
+    random.shuffle(colors)
+    return colors
