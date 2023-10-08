9f822d9139cc6cba01e2d94e79929530582a3874
beduffy <benfduffy@gmail.com>
2023-10-08 16:44:54 +0200

    Created OpenCvArucoImageText, chosen_corner_id

--- scripts/basic_aruco_example.py
+++ scripts/basic_aruco_example.py
@@ -42,22 +42,58 @@ from lib.aruco_helper import create_aruco_params, aruco_detect_draw_get_transfor
 #     return cam2arm, arm2cam, roll_marker, pitch_marker, yaw_marker, roll_camera, pitch_camera, yaw_camera
 
 
+class OpenCvArucoImageText():
+    def __init__(self) -> None:
+        # Opencv text params
+        self.start_y = 30
+        self.jump_amt = 30
+        self.text_size = 1
+        #--- 180 deg rotation matrix around the x axis
+        self.R_flip       = np.zeros((3, 3), dtype=np.float32)
+        self.R_flip[0, 0] =  1.0
+        self.R_flip[1, 1] = -1.0
+        self.R_flip[2, 2] = -1.0
+        
+    def put_marker_text(self, tvec, roll_marker, pitch_marker, yaw_marker):
+        # TODO understand all of the below intuitively. 
+        # -- Print the tag position in camera frame
+        str_position = "MARKER Position x={:.5f}  y={:.5f}  z={:.5f}".format(tvec[0], tvec[1], tvec[2])
+        cv2.putText(camera_color_img, str_position, (0, self.start_y), font, self.text_size, (0, 255, 0), 2, cv2.LINE_AA)
+
+        # -- Print the marker's attitude respect to camera frame
+        str_attitude = "MARKER Attitude r={:.5f}  p={:.5f}  y={:.5f}".format(
+            math.degrees(roll_marker), math.degrees(pitch_marker),
+            math.degrees(yaw_marker))
+        cv2.putText(camera_color_img, str_attitude, (0, self.start_y + self.jump_amt * 1), font, self.text_size, (0, 255, 0), 2, cv2.LINE_AA)
+
+    def put_camera_text(self, pos_camera, roll_camera, pitch_camera, yaw_camera):
+        str_position = "CAMERA Position x={:.5f}  y={:.5f}  z={:.5f}".format(
+            pos_camera[0].item(), pos_camera[1].item(), pos_camera[2].item())
+        cv2.putText(camera_color_img, str_position, (0, self.start_y + self.jump_amt * 2), font, self.text_size, (0, 255, 0), 2, cv2.LINE_AA)
+
+        str_attitude = "CAMERA Attitude r={:.5f}  p={:.5f}  y={:.5f}".format(
+            math.degrees(roll_camera), math.degrees(pitch_camera),
+            math.degrees(yaw_camera))
+        cv2.putText(camera_color_img, str_attitude, (0, self.start_y + self.jump_amt * 3), font, self.text_size, (0, 255, 0), 2, cv2.LINE_AA)
+
+
+    def put_avg_marker_text(self, avg_6dof_pose):
+        str_marker_pose_avg = 'M pos: ({:.5f} {:.5f} {:.5f}), angles: ({:.5f} {:.5f} {:.5f})'.format(
+            avg_6dof_pose[0], avg_6dof_pose[1], avg_6dof_pose[2], avg_6dof_pose[3], avg_6dof_pose[4], avg_6dof_pose[5]
+        )
+
+        cv2.putText(camera_color_img, str_marker_pose_avg, (0, self.start_y + self.jump_amt * 4), font, self.text_size, (0, 255, 0), 2, cv2.LINE_AA)
+
+
 if __name__ == '__main__':
     depth_intrin, color_intrin, depth_scale, pipeline, align, spatial = setup_start_realsense()
 
     board, parameters, aruco_dict, marker_length = create_aruco_params()
+    chosen_corner_id = None
 
-    show_matplotlib_all_aruco(aruco_dict)
+    # show_matplotlib_all_aruco(aruco_dict)
 
-    # Opencv text params
-    start_y = 30
-    jump_amt = 30
-    text_size = 1
-    #--- 180 deg rotation matrix around the x axis
-    R_flip       = np.zeros((3, 3), dtype=np.float32)
-    R_flip[0, 0] =  1.0
-    R_flip[1, 1] = -1.0
-    R_flip[2, 2] = -1.0
+    opencv_aruco_image_text = OpenCvArucoImageText()    
 
     marker_pose_history = deque([], maxlen=100)
 
@@ -83,6 +119,9 @@ if __name__ == '__main__':
             corners, ids, all_rvec, all_tvec = aruco_detect_draw_get_transforms(gray_data, camera_color_img, aruco_dict, parameters, marker_length, camera_matrix, dist_coeffs)
 
             if ids is not None:
+                if chosen_corner_id is None:
+                    chosen_corner_id = ids[0][0]
+                    print('Chose {} as chosen_corner_id'.format(chosen_corner_id))
                 print(ids)
                 ids_list = [l[0] for l in ids.tolist()]
                 for list_idx, corner_id in enumerate(ids_list):
@@ -93,7 +132,7 @@ if __name__ == '__main__':
                     # TODO I need charuco or aruco board and/or multiple markers... Use board beside arm and then use that as initial guess
                 
                     # TODO I need to look at a specific id... How will I change this in future? and make it more variable and stuff? Remember first id I see?
-                    if corner_id == 2:
+                    if chosen_corner_id is not None and corner_id == chosen_corner_id:
                         tvec, rvec = tvec_aruco, rvec_aruco
 
                         # refactoring fail but a lesson in here. TODO
@@ -102,46 +141,21 @@ if __name__ == '__main__':
                         cam2arm, arm2cam, R_tc, R_ct, pos_camera = create_homogenous_transformations(tvec, rvec)
 
                         # -- Get the attitude in terms of euler 321 (Needs to be flipped first)
-                        roll_marker, pitch_marker, yaw_marker = rotationMatrixToEulerAngles(R_flip * R_tc)
+                        roll_marker, pitch_marker, yaw_marker = rotationMatrixToEulerAngles(opencv_aruco_image_text.R_flip * R_tc)
                         # -- Get the attitude of the camera respect to the frame
-                        roll_camera, pitch_camera, yaw_camera = rotationMatrixToEulerAngles(R_flip * R_ct)  # todo no flip needed?
+                        roll_camera, pitch_camera, yaw_camera = rotationMatrixToEulerAngles(opencv_aruco_image_text.R_flip * R_ct)  # todo no flip needed?
 
                         marker_pose_history.append((tvec[0], tvec[1], tvec[2], 
                                                     math.degrees(roll_marker), math.degrees(pitch_marker), math.degrees(yaw_marker)))
 
-                         
                         avg_6dof_pose = []
                         for idx in range(6):
                             marker_pose_history_idx_val = np.mean([v[idx] for v in marker_pose_history]).item()
                             avg_6dof_pose.append(marker_pose_history_idx_val)
 
-                        # TODO understand all of the below intuitively. 
-                        # -- Print the tag position in camera frame
-                        str_position = "MARKER Position x={:.5f}  y={:.5f}  z={:.5f}".format(tvec[0], tvec[1], tvec[2])
-                        cv2.putText(camera_color_img, str_position, (0, start_y), font, text_size, (0, 255, 0), 2, cv2.LINE_AA)
-
-                        # -- Print the marker's attitude respect to camera frame
-                        str_attitude = "MARKER Attitude r={:.5f}  p={:.5f}  y={:.5f}".format(
-                            math.degrees(roll_marker), math.degrees(pitch_marker),
-                            math.degrees(yaw_marker))
-                        cv2.putText(camera_color_img, str_attitude, (0, start_y + jump_amt * 1), font, text_size, (0, 255, 0), 2, cv2.LINE_AA)
-
-                        str_position = "CAMERA Position x={:.5f}  y={:.5f}  z={:.5f}".format(
-                            pos_camera[0].item(), pos_camera[1].item(), pos_camera[2].item())
-                        cv2.putText(camera_color_img, str_position, (0, start_y + jump_amt * 2), font, text_size, (0, 255, 0), 2, cv2.LINE_AA)
-
-                        str_attitude = "CAMERA Attitude r={:.5f}  p={:.5f}  y={:.5f}".format(
-                            math.degrees(roll_camera), math.degrees(pitch_camera),
-                            math.degrees(yaw_camera))
-                        cv2.putText(camera_color_img, str_attitude, (0, start_y + jump_amt * 3), font, text_size, (0, 255, 0), 2, cv2.LINE_AA)
-
-                        str_marker_pose_avg = 'M pos: ({:.5f} {:.5f} {:.5f}), angles: ({:.5f} {:.5f} {:.5f})'.format(
-                            avg_6dof_pose[0], avg_6dof_pose[1], avg_6dof_pose[2], avg_6dof_pose[3], avg_6dof_pose[4], avg_6dof_pose[5]
-                        )
-
-                        cv2.putText(camera_color_img, str_marker_pose_avg, (0, start_y + jump_amt * 4), font, text_size, (0, 255, 0), 2, cv2.LINE_AA)
-
-
+                        opencv_aruco_image_text.put_marker_text(tvec, roll_marker, pitch_marker, yaw_marker)
+                        opencv_aruco_image_text.put_camera_text(pos_camera, roll_camera, pitch_camera, yaw_camera)
+                        opencv_aruco_image_text.put_avg_marker_text(avg_6dof_pose)
             
             images = np.hstack((camera_color_img, depth_colormap))
 
