308e8802bf1ed08ab24c61752f21058772f89198
beduffy <benfduffy@gmail.com>
2023-10-08 15:13:35 +0200

    Added create_charuco_params to aruco helpers, returned corners to detect draw get

--- lib/aruco_helper.py
+++ lib/aruco_helper.py
@@ -18,6 +18,28 @@ def create_aruco_params():
 
     return board, parameters, aruco_dict, marker_length
 
+def create_charuco_params():
+    parameters = aruco.DetectorParameters_create()
+    parameters.cornerRefinementMethod = aruco.CORNER_REFINE_SUBPIX
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    # charuco_marker_length = 0.019
+    charuco_marker_length = 0.0145
+    charuco_square_length = 0.028
+    # TODO maybe i can tune these more
+    # TODO find out how to do long distance pose estimation!!!
+
+    # TODO aruco con More susceptible to rotational ambiguity at medium to long ranges
+    # https://stackoverflow.com/questions/52222327/improve-accuracy-of-pose-with-bigger-aruco-markers
+    # https://stackoverflow.com/questions/51709522/unstable-values-in-aruco-pose-estimation
+    # TODO try apriltag_ros
+
+    # board = cv2.aruco.CharucoBoard_create(7, 7, .025, .0125, aruco_dict)
+    # board = cv2.aruco.CharucoBoard_create(7, 7, .025, .0125, dictionary)
+    # img = board.draw((200 * 3, 200 * 3))
+    board = cv2.aruco.CharucoBoard_create(7, 7, charuco_square_length, charuco_marker_length, aruco_dict)  # TODO test new values
+
+    return board, parameters, aruco_dict, charuco_marker_length
+
 
 # TODO get over the fear of creating classes
 def aruco_detect_draw_get_transforms(gray_data, camera_color_img, aruco_dict, parameters, marker_length, camera_matrix, dist_coeffs):
@@ -27,7 +49,8 @@ def aruco_detect_draw_get_transforms(gray_data, camera_color_img, aruco_dict, pa
     # TODO if there are two markers in image, it will not be more accurate right? or?
     all_rvec, all_tvec, _ = aruco.estimatePoseSingleMarkers(corners, marker_length, camera_matrix, dist_coeffs)
 
-    return ids, all_rvec, all_tvec
+    return corners, ids, all_rvec, all_tvec
+
 
 def show_matplotlib_all_aruco(aruco_dict):
     # TODO put into aruco helpers
