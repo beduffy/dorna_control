e21091062106562abf3b9c91f7fd3366f221f1e2
beduffy <benfduffy@gmail.com>
2023-10-10 12:12:43 +0200

    Added marker_separation variable and removed TODO

--- lib/aruco_helper.py
+++ lib/aruco_helper.py
@@ -9,14 +9,17 @@ def create_aruco_params():
     # marker_length = 0.028
     marker_length = 0.0275
     # marker_length = 0.0935  # big marker
+
+    marker_separation = 0.06
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
     parameters = aruco.DetectorParameters_create()
     parameters.cornerRefinementMethod = aruco.CORNER_REFINE_SUBPIX
     # parameters.adaptiveThreshWinSizeMin = 3
     # parameters.adaptiveThreshWinSizeStep = 4  # todo test more and see if it makes worse/better
-    board = cv2.aruco.GridBoard_create(3, 4, marker_length, 0.06, aruco_dict)  # marker_separation 0.06
+    board = cv2.aruco.GridBoard_create(3, 4, marker_length, marker_separation, aruco_dict)
 
     return board, parameters, aruco_dict, marker_length
+
 
 def create_charuco_params():
     parameters = aruco.DetectorParameters_create()
@@ -49,7 +52,6 @@ def aruco_detect_draw_get_transforms(gray_data, camera_color_img, aruco_dict, pa
     corners, ids, rejectedImgPoints = aruco.detectMarkers(gray_data, aruco_dict,
                                                                 parameters=parameters)
     frame_markers = aruco.drawDetectedMarkers(camera_color_img, corners, ids)  # TODO separate elsewhere? This function does too much?
-    # TODO if there are two markers in image, it will not be more accurate right? or?
     all_rvec, all_tvec, _ = aruco.estimatePoseSingleMarkers(corners, marker_length, camera_matrix, dist_coeffs)
 
     return corners, ids, all_rvec, all_tvec
