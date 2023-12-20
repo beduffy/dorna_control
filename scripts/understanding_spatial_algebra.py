21baaa9ab8f2497109bcd3d6313e9645d05ad1c9
beduffy <benfduffy@gmail.com>
2023-12-20 20:33:24 +0000

    Created understanding_spatial_algebra.py

---
+++ scripts/understanding_spatial_algebra.py
@@ -0,0 +1,89 @@
+from __future__ import print_function
+import sys
+import traceback
+from glob import glob
+import math
+from collections import deque
+
+try:
+    import open3d as o3d
+    from skimage.measure import find_contours
+except Exception as e:
+    print(e)
+    print('Tried to import open3d or skimage but not installed')
+import pyrealsense2 as rs
+from cv2 import aruco
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import matplotlib as mpl
+from scipy import optimize
+
+from lib.vision import euler_yzx_to_axis_angle, rotationMatrixToEulerAngles, create_homogenous_transformations
+
+mesh_box_opt = o3d.geometry.TriangleMesh.create_box(width=0.1, height=0.1, depth=0.003)
+mesh_box_opt.paint_uniform_color([1, 0, 0])
+
+size = 0.1
+origin_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=size, origin=[0.0, 0.0, 0.0])
+coordinate_frame_1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=size, origin=[0.3, 0.0, 0.0])
+coordinate_frame_2 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=size, origin=[0.4, 0.0, 0.0])
+coordinate_frame_3 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=size, origin=[0.5, 0.0, 0.0])
+
+camera_coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=size * 1.3, origin=[0.0, 0.0, 0.0])
+
+# TODO we can obviously go from rotation axis -> rotation matrix -> euler angles (two versions). But how to go backwards?
+
+# rvec = np.array([1.0, 1.0, 1.0])
+# z, x, y, angle = euler_yzx_to_axis_angle(np.pi / 2, 0, 0)  # pointing left in yaw, 45 deg. red.
+# z, x, y, angle = euler_yzx_to_axis_angle(0, np.pi / 2, 0)  # pointing up in pitch 45 degrees. 
+# z, x, y, angle = euler_yzx_to_axis_angle(0, 0, np.pi / 2)  # rolling right 45 degrees.
+
+# I want camera to the right, pointing to the left 45 degrees left in yaw and 45 degrees down
+z, x, y, angle = euler_yzx_to_axis_angle(np.pi / 2, -np.pi / 2, 0)
+rvec = np.array([x, y, z])
+camera_position_rel_to_origin = np.array([0.0, 0.3, 0.4])
+tvec = camera_position_rel_to_origin   # TODO why does 0.3 bring it down but 
+cam2_arm, arm2_cam, R_tc, R_ct, pos_camera = create_homogenous_transformations(tvec, rvec)
+# camera_transformation = np.array(cam2_arm)
+camera_transformation = np.array(arm2_cam)  # makes more intuitive sense, 0.3 tvec brings bigger coordinate frame forwards. 
+
+camera_coordinate_frame.transform(camera_transformation)
+
+#--- 180 deg rotation matrix around the x axis
+R_flip       = np.zeros((3, 3), dtype=np.float32)
+R_flip[0, 0] =  1.0
+R_flip[1, 1] = -1.0
+R_flip[2, 2] = -1.0
+
+# TODO I need to understand conversion between all rotation types better
+# TODO why are both below the same?
+# -- Get the attitude in terms of euler 321 (Needs to be flipped first)
+roll_marker, pitch_marker, yaw_marker = rotationMatrixToEulerAngles(R_flip * R_tc)
+print('roll pitch yaw marker: ', roll_marker, pitch_marker, yaw_marker)
+# -- Get the attitude of the camera respect to the frame
+roll_camera, pitch_camera, yaw_camera = rotationMatrixToEulerAngles(R_flip * R_ct)  # todo no flip needed?
+print('roll pitch yaw camera: ', roll_camera, pitch_camera, yaw_camera)
+
+
+
+estimated_camera_coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=size * 1.5, origin=[0.0, 0.0, 0.0])
+# estimated_camera_coordinate_frame.transform(estimated_cam2_arm_transform)
+
+
+arm_position_coord_frames = [coordinate_frame_1, coordinate_frame_2, coordinate_frame_3]
+list_of_geometry_elements = [origin_frame, camera_coordinate_frame, estimated_camera_coordinate_frame] + arm_position_coord_frames
+o3d.visualization.draw_geometries(list_of_geometry_elements)
+
+
+# TODO the big goal here would be to:
+# 1. Using the 3 transformations from origin to the 3 coord frames of "fake arm" and then # TODO calculate simple. Identity rotation mat and translation 0.3, 0.4, 0.5
+# 2. 3 transformations from camera to each of these coord frames. TODO how do I find these transformations. Translation is easy. Rotation is same as R_tc or R_ct? Can always prove I have things correct
+# 3. Use opencv handeye function and then plot output transformation? 
+
+'''
+Can i do all of this in simulation without even using realsense? 
+E.g. Put all objects and ground truth into simulation, visualise, then compute. 
+Use Drake or Open3D? Well putting aruco into these is hard. But I don’t need to? I have all the transforms. I don’t even need to visualise depth cameras.
+'''
