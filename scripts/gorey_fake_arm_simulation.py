586c9c0c55aaa30ec7c5e1c01926a710d3795f31
beduffy <benfduffy@gmail.com>
2023-12-20 20:30:50 +0000

    Put euler_yzx_to_axis_angle to vision.py

--- scripts/gorey_fake_arm_simulation.py
+++ scripts/gorey_fake_arm_simulation.py
@@ -20,7 +20,7 @@ from mpl_toolkits import mplot3d
 import matplotlib as mpl
 from scipy import optimize
 
-from lib.vision import isclose, dist, isRotationMatrix, rotationMatrixToEulerAngles, create_homogenous_transformations
+from lib.vision import euler_yzx_to_axis_angle, rotationMatrixToEulerAngles, create_homogenous_transformations
 
 # https://www.geeksforgeeks.org/calibratehandeye-python-opencv/
 gripper_t = np.array([[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [ 
@@ -56,43 +56,6 @@ HandEyeCalibrationMethod 	method = CALIB_HAND_EYE_TSAI
 
 print(R)
 print(T)
-
-
-
-
-
-# TODO put into transformations library and same with rotationMatrixToEulerAngles, create_homogenous_transformations
-# https://stackoverflow.com/questions/58997792/how-do-you-convert-euler-angles-to-the-axis-angle-representation-in-python
-def euler_yzx_to_axis_angle(z_e, x_e, y_e, normalize=True):
-    # Assuming the angles are in radians.
-    c1 = math.cos(z_e/2)
-    s1 = math.sin(z_e/2)
-    c2 = math.cos(x_e/2)
-    s2 = math.sin(x_e/2)
-    c3 = math.cos(y_e/2)
-    s3 = math.sin(y_e/2)
-    c1c2 = c1*c2
-    s1s2 = s1*s2
-    w = c1c2*c3 - s1s2*s3
-    x = c1c2*s3 + s1s2*c3
-    y = s1*c2*c3 + c1*s2*s3
-    z = c1*s2*c3 - s1*c2*s3
-    angle = 2 * math.acos(w)
-    if normalize:
-        norm = x*x+y*y+z*z
-        if norm < 0.001:
-            # when all euler angles are zero angle =0 so
-            # we can set axis to anything to avoid divide by zero
-            x = 1
-            y = 0
-            z = 0
-        else:
-            norm = math.sqrt(norm)
-            x /= norm
-            y /= norm
-            z /= norm
-    return z, x, y, angle
-# print(euler_yzx_to_axis_angle(1.2, 1.5, 1.0))
 
 
 mesh_box_opt = o3d.geometry.TriangleMesh.create_box(width=0.1, height=0.1, depth=0.003)
