8136e9472f9b88f2ee3eafc6531a3a8c2837c34e
beduffy <benfduffy@gmail.com>
2024-10-28 12:49:10 +0100

    Added new october 28th intrinsics 0.217 error whereas october 27th was 0.79 pixels...

--- lib/vision_config.py
+++ lib/vision_config.py
@@ -27,17 +27,23 @@ if open3d_import_success:
     
 # TODO measure reprojection error of this. And compare why the above is so different?
 # old intrinsic calibration done by me
-camera_matrix = np.array([[612.14801862, 0., 340.03640321],
-                            [0., 611.29345062, 230.06928807],
-                            [0., 0., 1.]])
-dist_coeffs = np.array(
-    [1.80764862e-02, 1.09549436e+00, -3.38044260e-03, 4.04543459e-03, -4.26585263e+00])
+# camera_matrix = np.array([[612.14801862, 0., 340.03640321],
+#                             [0., 611.29345062, 230.06928807],
+#                             [0., 0., 1.]])
+# dist_coeffs = np.array(
+#     [1.80764862e-02, 1.09549436e+00, -3.38044260e-03, 4.04543459e-03, -4.26585263e+00])
 
-# october 27 2024 directly from using grippers
+# october 27 2024 directly from using grippers. easured later 0.79 pixels reprojection error!!!
 # camera_matrix = np.array([[639.97875221,   0.        , 319.90874536],
 #        [  0.        , 479.91357711, 239.9565378 ],
 #        [  0.        ,   0.        ,   1.        ]])
 # dist_coeffs = np.array([[-0.00025456,  0.0004778 ,  0.00003447, -0.00002476, -0.00068842]])
+
+# october 28th, 21 images,  mean_error: 0.21742727005639262. But maybe I don't have enough angles?
+camera_matrix = np.array([[611.61332568  , 0.    ,     340.21606321],
+                        [  0.      ,   610.36491859 ,239.2910932 ],
+                        [  0.    ,       0.      ,     1.        ]])
+dist_coeffs = np.array([ 0.09702268, -0.04537982,  0.00111311,  0.00708548, -0.5604325 ])
 
 
 # directly from depth/color intrinsics from factory
